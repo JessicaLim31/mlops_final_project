9a6b7db7c19354b3a5a3d91452ac9be3b6bc2aea
Jessica Lim <lim09018@stthomas.edu>
2026-05-09 01:33:13 -0500

    add queue url

--- consumer/settings.py
+++ consumer/settings.py
@@ -1,6 +1,6 @@
 import os
 
-QUEUE_URL = os.getenv("QUEUE_URL", "https://sqs.us-east-1.amazonaws.com/YOUR_ACCOUNT/your-queue")
+QUEUE_URL = os.getenv("QUEUE_URL", "https://sqs.us-east-1.amazonaws.com/701262207008/test_queue")
 
 S3_BUCKET = os.getenv("S3_BUCKET", "mlops-final-project331")
 S3_MODEL_KEY = "models/latest/model.pkl"
