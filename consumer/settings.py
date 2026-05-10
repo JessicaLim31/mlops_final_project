937a087fd3bdf2bb58511c4cfd954e656fd1529e
JessicaLim31 <lim09018@stthomas.edu>
2026-05-10 16:41:30 -0500

    Update settings.py

--- consumer/settings.py
+++ consumer/settings.py
@@ -1,12 +1,11 @@
 import os
+import sys
 
-QUEUE_URL = os.getenv("QUEUE_URL", "https://sqs.us-east-1.amazonaws.com/701262207008/test-queue") # Enter your queue URL
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+from config import S3_BUCKET, QUEUE_URL
 
-S3_BUCKET = os.getenv("S3_BUCKET", "mlops-final-project331")
 S3_MODEL_KEY = "models/latest/model.pkl"
 PREDICTION_PREFIX =  "predictions/"
-
-
 MAX_MESSAGES = int(os.getenv("MAX_MESSAGES", "3"))
 VISIBILITY_TIMEOUT = int(os.getenv("VISIBILITY_TIMEOUT", "30"))
 WAIT_TIME = int(os.getenv("WAIT_TIME", "20"))
